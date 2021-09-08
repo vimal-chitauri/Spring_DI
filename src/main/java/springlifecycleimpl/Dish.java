85402a9335b41b7c5395187e335906456e23d2d1
vimalsingh <1234>
2021-09-08 23:15:46 +0530

    Implement SpringLifeCycle.xml using 1. XML 2. Interface 3. Annotation

--- src/main/java/springlifecycleimpl/Dish.java
+++ src/main/java/springlifecycleimpl/Dish.java
@@ -1,6 +1,12 @@
 package springlifecycleimpl;
 
-public class Dish {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class Dish /*implements InitializingBean, DisposableBean*/ {
     private String dishName;
 
     @Override
@@ -18,11 +24,42 @@ public class Dish {
         this.dishName = dishName;
     }
 
-    public void init(){
+    /**
+     * used by XML Configuration Technique
+     * @throws Exception
+     */
+    /*public void init(){
         System.out.println("init method called");
     }
 
     public void destroy(){
         System.out.println("destroy mehtod called");
+    }*/
+
+    /**
+     * used by Interface Technique
+     * @throws Exception
+     */
+   /* @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("init method called from afterPropertiesSet()");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Destroy method called from destroy() of DisposableBean");
+    }*/
+
+    /**
+     * used by Annnotation Technique
+     */
+    @PostConstruct
+    public void start() {
+        System.out.println("start method called");
+    }
+
+    @PreDestroy
+    public void end() {
+        System.out.println("end mehtod called");
     }
 }
--- src/main/java/springlifecycleimpl/SpringLifeCycleClass.java
+++ src/main/java/springlifecycleimpl/SpringLifeCycleClass.java
@@ -5,11 +5,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringLifeCycleClass {
     public static void main(String[] args) {
+        /**
+         * there are 3 ways to create Spring lifecycle method
+         * 1.xml way i.e to use AbstractApplicationContext and registerShutdownHook()
+         * 2.interface way i.e to use InitializingBean, DisposableBean interface inside bean class
+         * 3. Annotation way
+         */
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("SpringLifeCycle.xml");
-        Dish dish= (Dish) context.getBean("dish");
+        Dish dish = (Dish) context.getBean("dish");
         System.out.println(dish);
         context.registerShutdownHook();
+//   To use Destroy() for spring Lifecycle use AbstractApplicationContext class and context.registerShutdownHook();
 
-//        To use Destroy() for spring Lifecycle use AbstractApplicationContext class and context.registerShutdownHook();
+
+
     }
 }
